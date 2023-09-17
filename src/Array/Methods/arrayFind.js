791dec0721462cfbc51ceafb5966be375d5cd7ed
PrabathShanakaGurusinghe <psgatwork@gmail.com>
2023-09-17 12:58:31 +0530

    Finds

--- src/Array/Methods/arrayFind.js
+++ src/Array/Methods/arrayFind.js
@@ -4,3 +4,7 @@ let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // මෙහිදී, 3ක් අඩු කල විට 2ක් ලැබෙන්නේ අඥාත අගය 5ක් වූ විටය.
 let firstValue = num.find((x) => x - 3 === 2);
 console.log(firstValue); // output 5
+
+// Example 2 - 20ට වැඩි පළමු අගය සොයන්න
+let randomNum = [6, 2, 45, 23, 3, 5];
+console.log(randomNum.find((x) => x > 20)); //output 45
---
+++ src/Array/Methods/arrayFindLastIndex.js
@@ -0,0 +1,11 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// අදාල condition එක සපුරන, Array last element එකේ index එක ලබා දේ
+// මෙහිදී, 3ක් අඩු කල විට 2ක් ලැබෙන්නේ අඥාත අගය 5ක් වූ විටය.එහි index එක 4ය
+let firstValue = num.findIndex((x) => x - 3 === 2);
+console.log(firstValue); // output 4
+
+// Example 2 - 20ට වැඩි අවසාන අගය සොයන්න
+// value = 83, එහි index එක 4ය
+let randomNum = [6, 2, 45, 23, 83, 5];
+console.log(randomNum.findIndex((x) => x > 20)); //output 4
