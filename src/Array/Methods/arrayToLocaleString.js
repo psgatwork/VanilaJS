64841fef1c4a6f64595ac2a012ba5c2dacd627a5
PrabathShanakaGurusinghe <psgatwork@gmail.com>
2023-10-01 12:49:06 +0530

    Added all array functions.

---
+++ src/Array/Methods/arrayToLocaleString.js
@@ -0,0 +1,9 @@
+const num = [10, 11, 12, 13, 14, 15, 16];
+const num2 = [10, 21, 11, 45, 28, 195, 134346];
+const numbersInString = ["one", "two", "three", "four", "five", "six"];
+const words = ["apple", "bat", "nut", "rat", "cat"];
+
+console.log(num.toLocaleString()); //"10, 11, 12, 13, 14, 15, 16"
+console.log(num2.toLocaleString()); //"10,21,11,45,28,195,134,346"
+console.log(numbersInString.toLocaleString()); //"one,two,three,four,five,six"
+console.log(words.toLocaleString()); //"apple,bat,nut,rat,cat"
---
+++ src/Array/Methods/arrayToReversed.js
@@ -0,0 +1,4 @@
+const num = [10, 11, 12, 13, 14, 15, 16];
+
+// latest function please make sure browser is updated
+console.log(Array.toReversed()); // Result [16, 15, 14, 13, 12, 11, 10]
